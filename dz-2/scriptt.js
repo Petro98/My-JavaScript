274adc41f4b1a4ea92c70b1b78192d72b4fb936b
Petro98 <71210382+Petro98@users.noreply.github.com>
2021-06-06 20:33:10 +0300

    first commit1

--- dz-2/scriptt.js
+++ dz-2/scriptt.js
@@ -1,19 +1,19 @@
-//1. програму яка додає 3 числа
-// let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
-// alert(a + ' Тип ' + typeof a);
+// 1. програму яка додає 3 числа
+let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
+alert(a + ' Тип ' + typeof a);
 
 
 
 // 2. просто програма 
 let b = +prompt('Ведіть число',) + +prompt('Ведіть число',);
-let d = (b <= 0) ? NaNт : b;
+let d = (b <= 0) ? NaN : b;
 
 switch (d) {
 	case 1:
 	case 2:
 	case 3:
 	case 4:
-		alert(`${d} число більше 0 , але менше 5`);
+		alert(`${d} більше 0 , але менше 5`);
 		break;
 	case 5:
 		alert(`${d} число рівне 5`);
@@ -22,19 +22,19 @@ switch (d) {
 	case 7:
 	case 8:
 	case 9:
-		alert(`${d} число більше 5 , але менше 10`);
+		alert(`${d} більше 5 , але менше 10`);
 		break;
 	case 10:
-		alert(`${d} число рівне 10`);
+		alert(`${d} рівне 10`);
 		break;
 	case 11:
 	case 12:
 	case 13:
 	case 14:
-		alert(`${d} число більше 10 , але менше 15`);
+		alert(`${d} більше 10 , але менше 15`);
 		break;
 	case 15:
-		alert(`${d} число рівне 15`);
+		alert(`${d} рівне 15`);
 		break;
 	default:
 		alert(d);
