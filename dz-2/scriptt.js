b2c81acabb6fe6f514f491f2d2ec2e25213f2f40
Petro98 <71210382+Petro98@users.noreply.github.com>
2021-06-06 18:09:07 +0300

    first commit1

--- dz-2/scriptt.js
+++ dz-2/scriptt.js
@@ -1,12 +1,13 @@
-// let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
+let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
 
-// alert(a + ' Тип результату ' + typeof a);
-// alert(typeof a);
+alert(a + ' Тип результату ' + typeof a);
+alert(typeof a);
+
+
 
 
 let b = +prompt('Ведіть число',) + +prompt('Ведіть число',);
 let d = (b <= 0) ? NaN : b;
-// alert(d);
 
 switch (d) {
 	case 1:
@@ -14,34 +15,40 @@ switch (d) {
 	case 3:
 	case 4:
 		alert(`${d} число більше 0 , але менше 5`)
+		break
 	case 5:
 		alert(`${d} число рівне 5`)
+		break
 	case 6:
 	case 7:
 	case 8:
 	case 9:
 		alert(`${d} число більше 5 , але менше 10`)
+		break
 	case 10:
 		alert(`${d} число рівне 10`)
+		break
 	case 11:
 	case 12:
 	case 13:
 	case 14:
 		alert(`${d} число більше 10 , але менше 15`)
+		break
 	case 15:
 		alert(`${d} число рівне 15`)
+		break
 	default:
 		alert(d)
 }
 
-// // периметр рівносторонього трикутника
-// let p = +prompt('Периметр рівностороннього трикутника',);
+// периметр рівносторонього трикутника
+let p = +prompt('Периметр рівностороннього трикутника',);
 
-// if (p > 5) {
-// 	alert(p * 3);
-// } else if (p <= 5) {
-// 	alert(`Ти що здурів ? число не може бути ${p}`)
-// } else {
-// 	alert('Ти що дурак?Веди число!!!')
-// }
+if (p > 5) {
+	alert(p * 3);
+} else if (p <= 5) {
+	alert(`Ти що здурів ? число не може бути ${p}`)
+} else {
+	alert('Ти що дурак?Веди число!!!')
+}
 
