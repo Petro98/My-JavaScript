9a3c4810e1a06708dd05006ab4a481299eec6ab5
Petro98 <71210382+Petro98@users.noreply.github.com>
2021-06-09 18:58:30 +0300

    first commit1

--- dz-3-1/script.js
+++ dz-3-1/script.js
@@ -1,8 +1,17 @@
 
+
 function game() {
-	let a = +prompt();
+	let a = +prompt('ведіть початкове число не більше 3');
+	if (3 <= a || a <= -1 || a === 0) {
+				alert('Помилка');
+				const result = confirm('Продовжити гру??');
+				if (result) {
+					game();
+				} else {
+				}
+			} else {
 	let b = +prompt('ведіть число не більше 10');
-	if (10 <= b || b <= -1 || b === 0 ) {
+	if (10 <= b || b <= -1 || b === 0) {
 		alert('Помилка');
 	} else {
 		let want_1 = Math.floor(Math.random() * (a - b) + b);
@@ -36,5 +45,6 @@ function game() {
 			i--;
 		}
 	}
+}
 }
 
