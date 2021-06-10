1b5cded5d9374b1be870a95192933ab4cdc1d42c
Petro98 <71210382+Petro98@users.noreply.github.com>
2021-06-10 12:28:25 +0300

    first commit1

--- dz-2/scriptt.js
+++ dz-2/scriptt.js
@@ -1,59 +1,63 @@
 // 1. програму яка додає 3 числа
-// let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
-// alert(a + ' Тип ' + typeof a);
+let a = +prompt('Ведіть число',) + +prompt('Ведіть число',) + +prompt('Ведіть число',);
+alert(a + ' Тип ' + typeof a);
 
 
 
 // 2. просто програма 
-let b = +prompt('Ведіть число',) + +prompt('Ведіть число',);
-let a = "";
+let a = prompt('Ведіть число',);
+let d = prompt('Ведіть число',);
+let b = a + d;
 if (b.trim() == "") {
 	alert('Сума повинна бути више 0')
 } else if (b <= 0) {
 	alert('ведіть щось')
+} else if (isNaN(parseInt(b))) {
+	alert('ведіть')
 } else {
-	switch (b) {
+	let bb = +a + +d;
+	switch (bb) {
 		case 1:
 		case 2:
 		case 3:
 		case 4:
-			alert(`${b} більше 0 , але менше 5`);
+			alert(`${bb} більше 0 , але менше 5`);
 			break;
 		case 5:
-			alert(`${b} число рівне 5`);
+			alert(`${bb} число рівне 5`);
 			break;
 		case 6:
 		case 7:
 		case 8:
 		case 9:
-			alert(`${b} більше 5 , але менше 10`);
+			alert(`${bb} більше 5 , але менше 10`);
 			break;
 		case 10:
-			alert(`${d} рівне 10`);
+			alert(`${bb} рівне 10`);
 			break;
 		case 11:
 		case 12:
 		case 13:
 		case 14:
-			alert(`${b} більше 10 , але менше 15`);
+			alert(`${bb} більше 10 , але менше 15`);
 			break;
 		case 15:
-			alert(`${b} рівне 15`);
+			alert(`${bb} рівне 15`);
 			break;
 		default:
-			alert(d);
+			alert(bb);
 	}
 }
 
 
 // 3. периметр рівносторонього трикутника
-// let p = +prompt('Периметр рівностороннього трикутника',);
+let p = +prompt('Периметр рівностороннього трикутника',);
 
-// if (p > 5) {
-// 	alert(p * 3);
-// } else if (p <= 5) {
-// 	alert(`Ти що здурів ? число не може бути ${p}`);
-// } else {
-// 	alert('Ти що дурак?Веди число!!!');
-// }
+if (p > 5) {
+	alert(p * 3);
+} else if (p <= 5) {
+	alert(`Ти що здурів ? число не може бути ${p}`);
+} else {
+	alert('Ти що дурак?Веди число!!!');
+}
 
