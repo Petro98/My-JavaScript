4f4f5c15540e647c8e75686759d8dc702ce65158
Petro98 <71210382+Petro98@users.noreply.github.com>
2021-06-13 16:28:39 +0300

    first commit1

--- dz-5/script.js
+++ dz-5/script.js
@@ -12,8 +12,9 @@ console.log(arr.flat(Infinity));
 // 2.================================================
 // метод sort
 const array = [2, 5, 55, 88, 4, 77, 99,];
-console.log(array.sort((a, b) => a - b).join());
-console.log(array.sort((a, b) => b - a).join());
+console.log(array.sort((a, b) => a - b).join(''));
+console.log(array.sort((a, b) => b - a).toString());
+console.log(Math.max.apply(0 , array))
 
 
 // 3.================================================
@@ -22,3 +23,10 @@ const arrayOne = [1, 2, 3, 4,];
 let value = arrayOne.reduce((previous, item) => (previous + item + 5), 0);
 let arraySum = arrayOne.map((item) => (item + 10)).reduce((acc , curr) => (acc + curr) , 0);
 console.log(value ,arraySum);
+
+
+// 4 
+// найбільш застосовуємі методи
+// psuh , filter , sort , reduce , map
+// найменш застосувуємі мктоди
+// delete , toString , some ,indexOf , lastindexOf.
